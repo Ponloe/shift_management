befa68a212b012896dbef57da6cf1674d340b4eb
Soponloe <sovannsoponloe@gmail.com>
2025-04-03 17:56:35 +0700

    initial setup

---
+++ src/app/schedule/page.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export default function Page() {
+  return (
+    <div>
+      <h1>Welcome to the Page</h1>
+      <p>This is a basic Next.js page.</p>
+    </div>
+  );
+}
